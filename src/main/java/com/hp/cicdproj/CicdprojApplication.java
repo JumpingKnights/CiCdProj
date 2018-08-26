4301525ac7f82f7dc32e1fb952533e11142cfd0e
linhuiping <2wsxcde3>
2018-08-26 21:47:18 +0800

    初始项目

---
+++ src/main/java/com/hp/cicdproj/CicdprojApplication.java
@@ -0,0 +1,12 @@
+package com.hp.cicdproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdprojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdprojApplication.class, args);
+    }
+}
